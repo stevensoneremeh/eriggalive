30e38e0227cc7d27418f347ffdb088554794f5b1
stevensoneremeh <techcitybystevenson@zohomail.com>
2025-07-25 16:49:04 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   themeColor: "#000000",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
-    generator: 'v0.dev'
+    generator: 'stevensoneremeh'
 }
 
 export default function RootLayout({
